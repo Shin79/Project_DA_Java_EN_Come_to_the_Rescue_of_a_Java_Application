68ed95714ec9e6f77e84b7619cb09139ad3e0605
Shin <sg.goto@gmail.com>
2020-10-24 19:01:14 +0200

    Séparation des méthodes de tri  et d'écriture dans une nouvelle classe

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -40,49 +40,4 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 
 		return result;
 	}
-
-	/**
-	 * Sort and count occurences of each symptom
-	 * @param list of symptoms
-	 * @return map of symptoms/occurences
-	 */
-	public Map<String,Integer>countOccurences(List<String> symptoms){
-		
-		Map<String,Integer> map = new TreeMap<>();
-		
-		if (symptoms != null && !symptoms.isEmpty()) {
-			Collections.sort(symptoms);
-			
-			for (String symptom : symptoms) {
-				map.put(symptom, map.containsKey(symptom) ? map.get(symptom) + 1 : 1);
-			}
-		}
-		return map;
-	}
-}
-
-/**
-* Write in result.out
-* @param map with symptoms
-* @return "result.out"
-*/
-public void writeSymptoms(Map<String,Integer> mapSymptoms) throws IOException{
-	
-	FileWriter writer = new FileWriter("result.out");
-	
-	if(mapSymptoms != null &&  !mapSymptoms.isEmpty()) {
-		for(Map.Entry<String,Integer> m : mapSymptoms.entrySet()) {
-			try {
-				writer.write(m.getKey() + " : " + m.getValue() + "\n");
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("exportation terminée");
-		writer.close();
-	} else {
-		writer.write("No symptom");
-		writer.close();
-	}
-}
 }
