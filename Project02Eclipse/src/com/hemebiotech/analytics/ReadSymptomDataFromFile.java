0d7f71b89de4f65ab61adfb29f1ba5cfda06b0fe
Shin <sg.goto@gmail.com>
2020-10-27 17:56:04 +0100

    Suppression de packages non utilisés

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -2,13 +2,10 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+
 
 
 /**
