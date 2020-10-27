7560df36e068576c73a01f7bf410905d65fb2fde
Shin <sg.goto@gmail.com>
2020-10-27 17:59:05 +0100

    Suppression du package Collections

--- Project02Eclipse/src/com/hemebiotech/analytics/SortAndCountOccurences.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SortAndCountOccurences.java
@@ -3,7 +3,7 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.Collections;
+
 
 public class SortAndCountOccurences {
 	/**
